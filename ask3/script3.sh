0cd7627e3829af3b6dffa65d72c6c2c30c04d7c2
SimosNeopoulos <simneo22@gmail.com>
2021-11-15 20:20:18 +0200

    Cleaned up the comments

--- ask3/script3.sh
+++ ask3/script3.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-#Bubble Sort function
+# Bubble Sort function
 function bubble_sort {   
     local MAX=${#NUM_ARRAY[@]}
     SIZE=${#NUM_ARRAY[@]}
@@ -28,14 +28,14 @@ function bubble_sort {
     done
 }
 
-#Function that prints help message to command line and then terminates the program
+# Function that prints help message to command line and then terminates the program
 function help() {
     echo "Help script to write"
     exit 0
 }
 
 
-#Checks wheter the user is asking for help
+# Checks wheter the user is asking for help
 if [ "$1" == "-h" ]
 then
     help
@@ -45,7 +45,7 @@ elif [ -z "$1" ] || [ -z "$2" ]
 then
     echo "Command line parameters not provided or incorect. Program terminated"
     exit 1
-#Checks whether text file provided in the command line exists
+# Checks whether text file provided in the command line exists
 elif [ ! -f "$1" ]
 then
     echo "Could not find a text file. Program terminated"
@@ -53,30 +53,30 @@ then
 fi
 
 
-#Stores the words of the file without duplicates
+# Stores the words of the file without duplicates
 WORDS=`grep -o -E '\w+' $1 | sort -u -f`
 
-#Array that will contain the words
+# Array that will contain the words
 WORD_ARRAY=()
-#Array that will contain the number of appearnces for each word 
+# Array that will contain the number of appearnces for each word 
 NUM_ARRAY=()
 
-#Accessing all the words from the file
+# Accessing all the words from the file
 for WORD in $WORDS
 do
-    #Number of apperences of the WORD in the text file ($1)
+    # Number of apperences of the WORD in the text file ($1)
     APEARENCES=`grep -o -i "$WORD" $1 | wc -l`
 
-    #Appending each word and the number of it's appearnces
-    #to the coresponding array
+    # Appending each word and the number of it's appearnces
+    # to the coresponding array
     WORD_ARRAY+=( $WORD )
     NUM_ARRAY+=( $APEARENCES )
 done
 
-#Sorting the arrays
+# Sorting the arrays
 bubble_sort "${NUM_ARRAY[@]}" "${WORD_ARRAY[@]}"
 
-#Printing the top ($2) words with most appearences
+# Printing the top ($2) words with most appearences
 for((i=0; i<$2; i++))
 do
     echo "${NUM_ARRAY[$i]} ${WORD_ARRAY[$i]}"
