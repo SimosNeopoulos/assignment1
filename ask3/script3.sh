a7cee4d23f48a856049678eaffd60c305365411f
SimosNeopoulos <simneo22@gmail.com>
2021-11-15 02:51:06 +0200

    Added Bubble Sort

--- ask3/script3.sh
+++ ask3/script3.sh
@@ -1,5 +1,34 @@
 #! /usr/bin/bash
 
+function bubble_sort()
+{   
+    local max=${#NUM_ARRAY[@]}
+    size=${#NUM_ARRAY[@]}
+    while ((max > 0))
+    do
+        local i=0
+        while ((i < max))
+        do
+            if [ "$i" != "$(($size-1))" ] #array will not be out of bound
+            then
+                if [ ${NUM_ARRAY[$i]} \< ${NUM_ARRAY[$((i + 1))]} ]
+                   then
+                   local t=${NUM_ARRAY[$i]}
+                   NUM_ARRAY[$i]=${NUM_ARRAY[$((i + 1))]}
+                   NUM_ARRAY[$((i + 1))]=$t
+                    
+                   local tf=${WORD_ARRAY[$i]}
+                   WORD_ARRAY[$i]=${WORD_ARRAY[$((i + 1))]}
+                  WORD_ARRAY[$((i + 1))]=$tf
+                 fi
+             fi
+            ((i += 1))
+        done
+        ((max -= 1))
+    done
+}
+
+
 WORDS="$(grep -o -E '\w+' test.txt | sort -u -f)"
 
 WORD_ARRAY=()
@@ -11,3 +40,4 @@ do
     WORD_ARRAY+=( $WORD )
     NUM_ARRAY+=( $APEARENCES )
 done
+bubble_sort "${NUM_ARRAY[@]}" "${WORD_ARRAY[@]}"
