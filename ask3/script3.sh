cd3efcce0863f8254e77df3e3e6e7c5bffba681f
SimosNeopoulos <simneo22@gmail.com>
2021-11-15 02:44:48 +0200

    Registirng all the words from a file and the num of their appearences to arrays

--- ask3/script3.sh
+++ ask3/script3.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/bash
 
-echo Hello World
+WORDS="$(grep -o -E '\w+' test.txt | sort -u -f)"
+
+WORD_ARRAY=()
+NUM_ARRAY=()
+
+for WORD in $WORDS
+do
+    APEARENCES="$(grep -o -i "$WORD" test.txt | wc -l)"
+    WORD_ARRAY+=( $WORD )
+    NUM_ARRAY+=( $APEARENCES )
+done
