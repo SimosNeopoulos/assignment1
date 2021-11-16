f2c98a65deb6388ebcba170b652e49729c97581a
SimosNeopoulos <simneo22@gmail.com>
2021-11-16 23:01:22 +0200

    The program can provide input for every setuation

--- ask1/script1a.sh
+++ ask1/script1a.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/bash
 
+function error {
+    echo "$1 FAILED" >/dev/stderr
+    exit 1
+}
+
+function compare {
+    if [[ "$1" != "$2" ]]
+    then
+    echo "$3"
+    fi
+}
 
 while IFS= read -r line || [[ -n "$line" ]]
 do
@@ -15,11 +26,12 @@ do
         url=$(head -n 1 "$file")
         if [[ "$line" == "$url" ]]
         then
+            found=true
             prevHash=$(md5sum "$file")
             echo "$url" > "$file"
-            wget -q -O - "$url" >> "$file"
+            wget -q -O - "$url" >> "$file" || error "$line"
             newHash=$(md5sum "$file")
-            found=true
+            compare "$prevHash" "$newHash" "$line"
             break
         fi
         done
@@ -28,8 +40,8 @@ do
     then
         num=$(($(ls ./data1a| wc -l)+1))
         echo "$line" > "./data1a/URL$num.txt"
-        wget -q -O - "$line" >> "./data1a/URL$num.txt"
-        echo "$line INIT"
+        wget -q -O - "$line" >> "./data1a/URL$num.txt" && echo "$line INIT" || error "$line"
+        
     fi
 done < "$1"
 
