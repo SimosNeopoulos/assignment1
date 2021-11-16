f9b9c5bc4121c90bf2124bb633b4f4edd63c0fa2
SimosNeopoulos <simneo22@gmail.com>
2021-11-16 22:24:44 +0200

    Stores the data from the URLs and get their hash

--- ask1/script1a.sh
+++ ask1/script1a.sh
@@ -5,11 +5,31 @@ while IFS= read -r line || [[ -n "$line" ]]
 do
     if [[ "$line" == *"#"* ]]
     then
-            continue
+        continue
+    fi 
+    found=false
+    if [ ! -z "$(ls -A ./data1a)" ]; then
+        
+        for file in ./data1a/URL*.txt
+        do
+        url=$(head -n 1 "$file")
+        if [[ "$line" == "$url" ]]
+        then
+            prevHash=$(md5sum "$file")
+            echo "$url" > "$file"
+            wget -q -O - "$url" >> "$file"
+            newHash=$(md5sum "$file")
+            found=true
+            break
+        fi
+        done
     fi
-    i=1
-    for file in ./data1a/URL*.txt
-    do
-        i=$(($i+1))
-    done
+    if ! $found
+    then
+        num=$(($(ls ./data1a| wc -l)+1))
+        echo "$line" > "./data1a/URL$num.txt"
+        wget -q -O - "$line" >> "./data1a/URL$num.txt"
+        echo "$line INIT"
+    fi
 done < "$1"
+
