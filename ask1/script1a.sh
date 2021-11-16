cdd625506ffc1ad83e37e4230f5d0b63bea8e55a
SimosNeopoulos <simneo22@gmail.com>
2021-11-16 21:07:31 +0200

    Can read URLs and text files from folder data1a

--- ask1/script1a.sh
+++ ask1/script1a.sh
@@ -3,7 +3,14 @@
 
 while IFS= read -r line || [[ -n "$line" ]]
 do
-    if [ "$line" == "#"* ]; then
-        continue
+    if [[ "$line" == *"#"* ]]
+    then
+            continue
     fi
+    i=1
+    for file in ./data1a/URL*.txt
+    do
+        
+        i=$(($i+1))
+    done
 done < "$1"
