6c1d210f659dc8f93addbb5a845ee8d75c3d1f27
SimosNeopoulos <simneo22@gmail.com>
2021-11-16 16:26:24 +0200

    Replaced *awk* commands with *while* loops

--- ask3/script3.sh
+++ ask3/script3.sh
@@ -25,7 +25,7 @@ then
     echo "Command line parameters not provided or incorect. Program terminated"
     exit 1
 # Checks whether text file provided in the command line exists
-elif [ ! -f "$1" ]
+elif [ ! -r "$1" ]
 then
     echo "Could not find a text file. Program terminated"
     exit 1
@@ -37,12 +37,39 @@ start="*** START OF THIS PROJECT GUTENBERG EBOOK"
 # The down limit of the file
 end="*** END OF THIS PROJECT GUTENBERG EBOOK"
 
-# Veriables that represent the upper and down limits of the text file
-firstLine="$(grep -n "$start" $1 | head -n 1 | cut -d: -f1)"
-lastLine="$(grep -n "$end" $1 | head -n 1 | cut -d: -f1)"
+# Boolean veriable that is preset as "false" and indicates
+# whether we are within the set bounds in the text file
+read=false
 
-# Creates a temporary text file that only contains the words that concern us
-awk -v a=$firstLine -v b=$lastLine 'NR==a+1, NR==b-1 {print $0}' $1 >> wordFile.txt
+# Reads the text file that has been passed as argument in the command line
+# and appends all the lines that are within bounds to the wordFile.txt file..
+while IFS= read -r line
+do
+  if $read
+  then
+    # When we reach the down bounds the loop breaks.
+    # Else the current line is appended to the wordFile.txt file
+    if [[ "$line" == *"$end"* ]]
+    then
+        break
+    else
+        echo "$line" >> wordFile.txt
+        continue
+    fi
+  fi
+  
+  # If the read veriable is false and "$start" is a substring of
+  # "$line" then we are now within bounds and $read is set to true
+  if ! $read
+  then
+    if [[ "$line" == *"$start"* ]]
+    then
+        read=true
+    fi
+  fi
+
+done < "$1"
+
 
 # Stores the words of the file without duplicates
 words=$(grep -o -E "(\w|')+" wordFile.txt | sed -e "s/'.*\$//" | sort -u -f)
@@ -51,7 +78,7 @@ words=$(grep -o -E "(\w|')+" wordFile.txt | sed -e "s/'.*\$//" | sort -u -f)
 for word in $words
 do
     # Number of apperences of the word in the text file (wordFile.txt)
-    apearences=`grep -o -i "$word" wordFile.txt | wc -l`
+    apearences=`grep -w -o -i "$word" wordFile.txt | wc -l`
 
     # Printing each word and the number of it's appearences in the data.txt file
     printf "%s %s\n" $apearences $word >> data.txt
@@ -66,8 +93,20 @@ sort -nr data.txt >> sorted.txt
 # Deleting data text file
 rm data.txt
 
-# Printing 
-awk -v end=$2 'NR==1, NR==end {print $2, $1}' sorted.txt
+# Counter that breaks the while loop once enought lines have been printed
+count=0
+
+# Printing the results
+while IFS= read -r line
+do
+    message=$(tac -s " " <<< "$line" )
+    echo  $message
+    count=$(($count+1))
+    if [ $count -ge $2 ]
+    then
+        break
+    fi
+done < sorted.txt
 
 # Deleting sorted text file
 rm sorted.txt
