c4ae46d5f8a4f2131baef7a5a902a585d0656ccc
SimosNeopoulos <simneo22@gmail.com>
2021-11-18 14:37:52 +0200

    Added comments to *script1b.sh* and formated *script1a.sh* code

--- ask1/script1a.sh
+++ ask1/script1a.sh
@@ -16,7 +16,7 @@ function compare {
 # If the data1a directory doesn't exist it is created here
 [ ! -d "./data1a" ] && mkdir data1a
 
-# A while loop that goes threw all the lines from the text file in the
+# A while loop that goes through all the lines from the text file in the
 # command line argument. If the line doesn't have a url (the line starts with "#")
 # it skips this line
 while IFS= read -r line || [[ -n "$line" ]]
@@ -34,45 +34,46 @@ do
     # Checks if the "data1a" directory is empty
     if [ ! -z "$(ls -A ./data1a)" ]; then
         
-        # Iterating threw the text files in the data1a directory
+        # Iterating through the text files in the data1a directory
         for file in ./data1a/URL*.txt
         do
-
-        # The url from the current file
-        url=$(head -n 1 "$file")
-        if [[ "$line" == "$url" ]]
-        then
-            found=true
-            
-            # Getting the hash from the file before it's contents are refreshed
-            prevHash=$(md5sum "$file")
-
-            # Reriting the url to the text file
-            echo "$url" > "$file"
-
-            # Veriable that indicated whether there was an error with the url or not
-            failed=false
-
-            # Getting the content from the current url ($url) and appends it to "$file"
-            # If an error occurs during the execution the veriable "failed" is set to "true"
-            wget -q -O - "$url" >> "$file" || failed=true
-
-            # Getting the hash from the file after it's contents are refreshed
-            newHash=$(md5sum "$file")
-
-            # If an error was raised with the url download then an error message is printed.
-            # If not the old hash, from the url's page, and the new are compered and an appropriate
-            # message is printed.
-            if $failed
+            # The url from the current file that was stored in the first line of the text file
+            url=$(head -n 1 "$file")
+            if [[ "$line" == "$url" ]]
             then
-                error "$line"
-            else
-                compare "$prevHash" "$newHash" "$line"
+                found=true
+            
+                # Getting the hash from the file before it's contents are refreshed
+                prevHash=$(md5sum "$file")
+
+                # Reriting the url to the text file
+                echo "$url" > "$file"
+
+                # Veriable that indicated whether there was an error with the url or not
+                failed=false
+
+                # Getting the content from the current url ($url) and appends it to "$file"
+                # If an error occurs during the execution the veriable "failed" is set to "true"
+                wget -q -O - "$url" >> "$file" || failed=true
+
+                # Getting the hash from the file after it's contents are refreshed
+                newHash=$(md5sum "$file")
+
+                # If an error was raised with the url download then an error message is printed.
+                # If not the old hash, from the url's page, and the new are compered and an appropriate
+                # message is printed.
+                if $failed
+                then
+                    error "$line"
+                else
+                    compare "$prevHash" "$newHash" "$line"
+                fi
+                break
             fi
-            break
-        fi
         done
     fi
+    
+    # If the html from the current url was't stored already it is being initialised here
     if ! $found
     then
         # The number of files+1 in the data1a directory
@@ -84,7 +85,6 @@ do
         # Getting the content from the current url ($line) and appends it to "./data1a/URL$num.txt"
         # If an error occurs during the execution an error message occurs
         wget -q -O - "$line" >> "./data1a/URL$num.txt" && echo "$line INIT" || error "$line"
-        
     fi
 done < "$1"
 
--- ask1/script1b.sh
+++ ask1/script1b.sh
@@ -14,6 +14,9 @@ function compare {
     fi
 }
 
+# This function gets the hash code from the file it is that has been passed as argument,
+# before and after it appends the new content from the url's web page.
+# It then calls the "compare" function to find whether somethinf has change or not.
 function append {
     local _url="$1"
     local _fileName="$2"
@@ -30,26 +33,29 @@ function append {
     fi
 }
 
+# Function that stores the data for the new urls
 function create {
     randName=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 5)
     echo "$1" > "./data1b/URL$randName.txt" 
     wget -q -O - "$1" >> "./data1b/URL$randName.txt" && echo "$1 INIT" || error "$1"
 }
 
+# Function that initialises
 function initialise {
-
+    # local array veriable that contains all the urls that are to be initialised
     local -n _urls=$1
+
+    # Iterting through _urls array and storing their data
     for _url in ${_urls[@]}   
-    do
+    do 
         create "$_url" &
     done
     wait
-    
 }
 
-
+# Function that iterates trough each stored url and it's file location
+# passing them as arguments for the "append" function.
 function check {
-
     local -n _urls=$1
     local -n _fileNames=$2
     local -i _num=${#_urls[@]}
@@ -63,26 +69,33 @@ function check {
 # If the data1b directory doesn't exist it is created here
 [ ! -d "./data1b" ] && mkdir data1b
 
-
+# Arrays that will store the urls for the arrays that are to be initilised
+# and the urls that are already stored and the file names with their location
 newURLs=()
 oldURLs=()
 oldFileName=()
 
-# A while loop that goes threw all the lines from the text file in the
+# A while loop that goes through all the lines from the text file in the
 # command line argument. If the line doesn't have a url (the line starts with "#")
 # it skips this line
 while IFS= read -r line || [[ -n "$line" ]]
 do
-    # If the data1b directory doesn't exist it is created here
+    # Boolean veriable that indivates whether a url with the same name
+    # as the one intereted now is already stored
     found=false
+
+    # If the line starts with a "#" it skips it
     if [[ "$line" == *"#"* ]]
     then
         continue
     else
+        # Checks if the "data1b" directory is empty
         if [ ! -z "$(ls -A ./data1b)" ]
         then
+            # Iterating through the text files in the data1a directory
             for file in ./data1b/URL*.txt
             do
+                # The url from the current file that was stored in the first line of the text file
                 url=$(head -n 1 "$file")
                 if [[ "$line" == "$url" ]]
                 then
@@ -93,6 +106,7 @@ do
                 fi
             done
         fi
+        # If the html from the current url was't stored already it's url address is added to "newURLs" array
         if ! $found
         then
             newURLs+=( "$line" )
@@ -100,11 +114,12 @@ do
     fi
 done < "$1"
 
-
+# Comparing the old web page instant with the new
 if ((${#oldURLs[@]})); then
     check oldURLs oldFileName &
 fi
 
+# Initialising the new urls
 if ((${#newURLs[@]})); then
     initialise newURLs &
 fi
