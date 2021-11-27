2303c0cd5167991381783615733a3bb3d77772dc
SimosNeopoulos <simneo22@gmail.com>
2021-11-27 17:06:49 +0200

    Unziping the command line argument and getting all the *.txt* files from it

--- askisi2/script2.sh
+++ askisi2/script2.sh
@@ -1,4 +1,33 @@
 #! /usr/bin/bash
 
+# Unziping the compressed file that has been passed as an argument
+# and echoing all the .txt files it contained.
+function unzip {
 
-echo Hello World
+    # Creating the directory in which all the contents from the 
+    # zipped file will be unziped
+    mkdir _unzipingLocation
+
+    # Unziping the contents into "./_unzipingLocation"
+    tar -xf "$1" -C ./_unzipingLocation
+
+    # Finding all the ".txt" files and outputing them with "echo"
+    textFiles=$(find ./_unzipingLocation -name "*.txt")
+    echo "$textFiles"
+
+    # Deleting the directory in which all the contents from the 
+    # zipped file were unziped and it's contents
+    rm -r _unzipingLocation
+}
+
+
+# Checks if a command line argument was provided
+if [ -z "$1" ]
+then
+    echo "Command line parameters not provided or incorect. Program terminated"
+    exit 1
+fi
+
+# Getting all the ".txt" files from the "$1" compressed file
+textFiles=$(unzip "$1")
+
