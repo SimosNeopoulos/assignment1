a67e2fa650e05b7b9a01c78ec74a13339261859e
SimosNeopoulos <simneo22@gmail.com>
2021-11-27 19:09:14 +0200

    Storing the repo urls in an array

--- askisi2/script2.sh
+++ askisi2/script2.sh
@@ -15,9 +15,7 @@ function unzip {
     textFiles=$(find ./_unzipingLocation -name "*.txt")
     echo "$textFiles"
 
-    # Deleting the directory in which all the contents from the 
-    # zipped file were unziped and it's contents
-    rm -r _unzipingLocation
+    
 }
 
 
@@ -31,3 +29,32 @@ fi
 # Getting all the ".txt" files from the "$1" compressed file
 textFiles=$(unzip "$1")
 
+# Array that stores the urls for the git repositories
+repo_urls=()
+
+# Iterating trough all the ".txt" files
+for file in $textFiles; do
+
+    # Reading the ".txt" file line by line,
+    # skiping the lines that don't start with "https", until the end of the file
+    # If a line starts with "https" we store the content of the line in "repo_urls" and exit the loop
+    while IFS= read -r line || [[ -n "$line" ]]; do
+
+        # If the line doesn't start with a "https" it skips it
+        if [[ "$line" != "https"* ]]; then
+            continue
+        else
+            repo_urls+=( "$line" )
+            break
+        fi
+    done < "$file"
+done
+
+# Deleting the directory in which all the contents from the 
+# zipped file were unziped and it's contents
+rm -r _unzipingLocation
+
+for repo in "${repo_urls[@]}"; do
+    # Add code to iterate the repositories and clone them here
+    echo "$repo"
+done
